2812bb46e69fc5256073c98c04e4ffbdc14d08c1
Arr_Wog <118063305+ArrWog@users.noreply.github.com>
2024-05-21 22:11:29 +0300

    Add files via upload
    Создание класса Human - добавление метода GetAge - для её дальнейшего вызова со значением

---
+++ Human.java
@@ -0,0 +1,15 @@
+public class Human {
+    int age = 25;
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getAge());
+
+
+    }
+
+    int getAge() {
+        return age;
+    }
+}
+
